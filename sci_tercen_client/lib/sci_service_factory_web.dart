0359692ac2fdda2f5446e889aeb5b2221a71bfd7
Alexandre Maurel <alexandre.maurel@gmail.com>
2025-11-05 14:02:29 +0100

    prepare 1.4.11

--- sci_tercen_client/lib/sci_service_factory_web.dart
+++ sci_tercen_client/lib/sci_service_factory_web.dart
@@ -1,19 +1,27 @@
 import 'package:sci_tercen_client/sci_client_service_factory.dart';
 import 'package:sci_tercen_client/sci_client.dart' as sci;
 import 'package:sci_tercen_client/sci_client_service_factory.dart' as tercen;
+import 'package:sci_http_client/http_client.dart' as http_client;
+import 'package:sci_http_client/http_auth_client.dart' as auth_http;
 
 import 'package:sci_http_client/http_browser_client.dart'
-if (dart.library.io) 'package:sci_http_client/http_io_client.dart';
+    if (dart.library.io) 'package:sci_http_client/http_io_client.dart';
 
-Future<ServiceFactory> createServiceFactoryForWebApp() async {
+Future<ServiceFactory> createServiceFactoryForWebApp(
+    {String? tercenToken}) async {
   // Set the HTTP client - conditional code based on platform
   _setHttpClient();
+
+  var httpClient = http_client.HttpClient();
+  if (tercenToken != null) {
+    httpClient = auth_http.HttpAuthClient(tercenToken);
+  }
 
   var factory = sci.ServiceFactory();
   var uriBase = Uri.base;
   await factory.initializeWith(
-    Uri(scheme: uriBase.scheme, host: uriBase.host, port: uriBase.port),
-  );
+      Uri(scheme: uriBase.scheme, host: uriBase.host, port: uriBase.port),
+      httpClient);
 
   tercen.ServiceFactory.CURRENT = factory;
 
@@ -23,7 +31,6 @@ Future<ServiceFactory> createServiceFactoryForWebApp() async {
 void _setHttpClient() {
   // This will use HttpIOClient or HttpBrowserClient depending on the platform
   try {
-    // Try web client first
     HttpBrowserClient.setAsCurrent();
   } catch (e) {
     throw "HttpBrowserClient is only available for web platform. This error indicates the code is running in a non-web environment.";
