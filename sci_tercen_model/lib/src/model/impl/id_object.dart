44f7f8f1d0980b5ce56d790074c2e25c996aac2c
Alexandre Maurel <alexandre.maurel@gmail.com>
2025-11-29 14:41:20 +0100

    add `propertyName` override to `IdObject` and `MetaFactor` for dynamic parent handling; ensure handling for `base.Base` and `List` parent types.

--- sci_tercen_model/lib/src/model/impl/id_object.dart
+++ sci_tercen_model/lib/src/model/impl/id_object.dart
@@ -3,4 +3,16 @@ part of sci_model;
 class IdObject extends IdObjectBase {
   IdObject() : super();
   IdObject.json(super.m) : super.json();
+
+  @override
+  String? get propertyName {
+    if (parent == null) return null;
+    dynamic baseParent = parent;
+    if (baseParent is base.Base) {
+      return super.propertyName;
+    } else if (baseParent is List) {
+      return '@[id="$id"]';
+    }
+    return null;
+  }
 }
--- sci_tercen_model/lib/src/model/impl/meta_factor.dart
+++ sci_tercen_model/lib/src/model/impl/meta_factor.dart
@@ -3,4 +3,16 @@ part of sci_model;
 class MetaFactor extends MetaFactorBase {
   MetaFactor() : super();
   MetaFactor.json(super.m) : super.json();
+
+  @override
+  String? get propertyName {
+    if (parent == null) return null;
+    dynamic baseParent = parent;
+    if (baseParent is base.Base) {
+      return super.propertyName;
+    } else if (baseParent is List) {
+      return '@[name="$name"]';
+    }
+    return null;
+  }
 }
